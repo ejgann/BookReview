64fca8236b09ea472638289de94b2353756c9ce4
Emily Gann <aspentree79@gmail.com>
2020-08-08 11:07:15 -0400

    built out books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,47 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to books_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @book = Book.find_by_id(params[:id])
+    end
+
+    def edit
+        @book = Book.find_by_id(params[:id])
+    end
+    
+    def update
+        @book = Book.find_by_id(params[:id])
+        if @book.update
+            redirect_to book_path(@book)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @book = Book.find_by_id(params[:id])
+        @book.delete
+        redirect_to books_path
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :description, :user_id, :category_id)
+    end
 end
