ea8e31ad659213a677f30260c141ecda34c56e2a
Emily Gann <aspentree79@gmail.com>
2020-08-08 11:31:52 -0400

    fixed error in routes; configured users controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+    def index
+    end
+    
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+        else
+            flash[:message] = "Sorry, please try again."
+            redirect_to login_path
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
 end
