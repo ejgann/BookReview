f167533f9bb260169575e4e6f0aa694b01fc4587
Emily Gann <aspentree79@gmail.com>
2020-08-08 11:43:01 -0400

    built out sessions views index and new; created nav bar with logic for logged in user; added helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        # returns current_user if already defined or instructs to find the user by id in the table if the user has been created already
+    end
+
+    def logged_in?
+        !!current_user
+    end
+    
+    def redirect_if_not_logged_in
+        redirect_to login_path if !logged_in?
+    end
+
 end
