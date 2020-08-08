d9187dbcdd25a3f65a508fdf887b06ec36f94caa
Emily Gann <aspentree79@gmail.com>
2020-08-08 12:01:48 -0400

    fixed some errors with signing in; started created views files for books

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,13 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
-        if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.id
-        else
-            flash[:message] = "Sorry, please try again."
-            redirect_to login_path
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to root_path
+            else
+                flash[:message] = "Sorry, please try again."
+                redirect_to login_path
+            end
     end
 
     def destroy
